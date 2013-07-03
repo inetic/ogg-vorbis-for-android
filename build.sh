cef9da4095b7a4b63b439b1953b931551342df6f
Peter Jankuliak <p.jankuliak@gmail.com>
2013-07-03 18:27:21 +0200

    Adds first set of files.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ogg_ver=1.3.1
+vor_ver=1.3.3
+
+if [ "$1" == clean ]; then
+  rm -rf obj
+  rm -rf libogg-$ogg_ver
+  rm -rf libogg-$ogg_ver.tar.gz
+  rm -rf libvorbis-$vor_ver
+  rm -rf libvorbis-$vor_ver.tar.gz
+  exit
+fi
+
+function unpack-and-configure {
+  what=$1
+  version=$2
+
+  rm -rf lib$what-$version
+  
+  if [ ! -f lib$what-$version.tar.gz ]; then
+    wget http://downloads.xiph.org/releases/$what/lib$what-$version.tar.gz
+  fi
+  
+  tar xf lib$what-$version.tar.gz
+
+  (cd lib$what-$version; ./configure --host=arm-android-linux)
+}
+
+unpack-and-configure ogg $ogg_ver
+unpack-and-configure vorbis $vor_ver
+
+ndk-build OGG_DIR=libogg-$ogg_ver VORBIS_DIR=libvorbis-$vor_ver
+
